fee008cf2a41786c06487ca3ef5b75a3bc4a4481
Andriu <andresllancapanh@gmail.com>
2025-04-23 21:33:05 -0400

    arreglo navbar

--- js/main.js
+++ js/main.js
@@ -25,10 +25,3 @@ window.addEventListener('scroll', () => {
         navbar.style.backdropFilter = 'none';
     }
 });
-
-const toggleBtn = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-toggleBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
