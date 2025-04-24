bc7b75758078b6715fa9cf6d330406ba8ae3215b
Andriu <andresllancapanh@gmail.com>
2025-04-23 21:16:18 -0400

    navbar hamburguesa

--- js/main.js
+++ js/main.js
@@ -25,3 +25,10 @@ window.addEventListener('scroll', () => {
         navbar.style.backdropFilter = 'none';
     }
 });
+
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
